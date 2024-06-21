94855f5504998779b33ee659c01bff479be71929
MikeszIPN <msanchezz2100@alumno.ipn.mx>
2024-06-20 23:35:43 -0600

    Mike

--- Mike/app.py
+++ Mike/app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import os
 
-from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings 
 from langchain.vectorstores import FAISS
